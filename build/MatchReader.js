8bd7ad41b325eae33950dbe731fb1735f66b77d1
Nishanth <>
2020-04-12 12:03:12 +0200

    Refactor Inheritance and composition

--- build/MatchReader.js
+++ build/MatchReader.js
@@ -1,9 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var MatchReader = /** @class */ (function () {
-    function MatchReader(reader) {
+var utils_1 = require("./utils");
+var GenericReader = /** @class */ (function () {
+    function GenericReader(reader) {
         this.reader = reader;
+        this.matches = [];
     }
-    return MatchReader;
+    GenericReader.prototype.load = function () {
+        this.matches = this.reader.data.map(this.mapRow);
+    };
+    GenericReader.prototype.mapRow = function (row) {
+        return [
+            utils_1.dateStringToDate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5],
+            row[6],
+        ];
+    };
+    return GenericReader;
 }());
-exports.MatchReader = MatchReader;
+exports.GenericReader = GenericReader;
--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,11 +1,7 @@
 import fs from 'fs';
-import { MatchResult } from './MatchResult';
-import { dateStringToDate } from './utils';
-
-type MatchData = [Date, string, string, number, number, MatchResult, string]; //Tuple data structure
 
 export class CsvFileReader {
-  data: MatchData[] = [];
+  data: string[][] = [];
   constructor(public filename: string) {}
 
   read(): void {
@@ -16,19 +12,6 @@ export class CsvFileReader {
       .split('\n')
       .map((row: string): string[] => {
         return row.split(',');
-      })
-      .map(this.mapRow);
-  }
-
-  mapRow(row: string[]): MatchData {
-    return [
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      row[5] as MatchResult, //type assertion
-      row[6],
-    ];
+      });
   }
 }
